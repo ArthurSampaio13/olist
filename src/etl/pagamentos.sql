f28670edb919a2b44ca6d7e0dd598175eb1e4ac6
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-10 10:03:54 -0300

    etl pagamento :white_check_mark:

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -34,106 +34,165 @@ with
         ORDER BY
             seller_id,
             payment_type
+    ),
+    tb_summary as (
+        select
+            seller_id,
+            1.0 * sum(
+                case
+                    when payment_type = 'credit_card' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) as qt_credit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'boleto' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) as qt_boleto,
+            1.0 * sum(
+                case
+                    when payment_type = 'debit_card' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) as qt_debit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'voucher' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) as qt_voucher,
+            1.0 * sum(
+                case
+                    when payment_type = 'credit_card' then payment_value
+                    else 0
+                end
+            ) as vl_credit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'boleto' then payment_value
+                    else 0
+                end
+            ) as vl_boleto,
+            1.0 * sum(
+                case
+                    when payment_type = 'debit_card' then payment_value
+                    else 0
+                end
+            ) as vl_debit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'voucher' then payment_value
+                    else 0
+                end
+            ) as vl_voucher,
+            1.0 * sum(
+                case
+                    when payment_type = 'credit_card' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) / sum(qtdPedidoMeioPagamento) as pct_qtd_credit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'boleto' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) / sum(qtdPedidoMeioPagamento) as pct_qtd_boleto,
+            1.0 * sum(
+                case
+                    when payment_type = 'debit_card' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) / sum(qtdPedidoMeioPagamento) as pct_qtd_debit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'voucher' then qtdPedidoMeioPagamento
+                    else 0
+                end
+            ) / sum(qtdPedidoMeioPagamento) as pct_qtd_voucher,
+            1.0 * sum(
+                case
+                    when payment_type = 'credit_card' then payment_value
+                    else 0
+                end
+            ) / sum(payment_value) as pct_vl_credit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'boleto' then payment_value
+                    else 0
+                end
+            ) / sum(payment_value) as pct_vl_boleto,
+            1.0 * sum(
+                case
+                    when payment_type = 'debit_card' then payment_value
+                    else 0
+                end
+            ) / sum(payment_value) as pct_vl_debit_card,
+            1.0 * sum(
+                case
+                    when payment_type = 'voucher' then payment_value
+                    else 0
+                end
+            ) / sum(payment_value) as pct_vl_voucher
+        from
+            tb_group
+        GROUP BY
+            seller_id
+    ),
+    OrderedPayments AS (
+        SELECT
+            seller_id,
+            payment_installments,
+            ROW_NUMBER() OVER (
+                PARTITION BY
+                    seller_id
+                ORDER BY
+                    payment_installments
+            ) AS row_num,
+            COUNT(*) OVER (
+                PARTITION BY
+                    seller_id
+            ) AS total_count
+        FROM
+            tb_join
+        WHERE
+            payment_type = 'credit_card'
+    ),
+    MedianCalc AS (
+        SELECT
+            seller_id,
+            payment_installments,
+            row_num,
+            total_count,
+            (total_count + 1) / 2 AS mid_point1,
+            (total_count + 2) / 2 AS mid_point2
+        FROM
+            OrderedPayments
+    ),
+    tb_cartao as (
+        SELECT
+            seller_id,
+            AVG(payment_installments) AS avgQtdParcelas,
+            AVG(
+                CASE
+                    WHEN row_num IN (mid_point1, mid_point2) THEN payment_installments
+                END
+            ) AS medianaQtdParcelas,
+            max(payment_installments) as maxQtdParcelas,
+            min(payment_installments) as minQtdParcelas
+        FROM
+            MedianCalc
+        GROUP BY
+            seller_id
     )
 select
-    seller_id,
-    1.0 * sum(
-        case
-            when payment_type = 'credit_card' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) as qt_credit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'boleto' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) as qt_boleto,
-    1.0 * sum(
-        case
-            when payment_type = 'debit_card' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) as qt_debit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'voucher' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) as qt_voucher,
-    1.0 * sum(
-        case
-            when payment_type = 'credit_card' then payment_value
-            else 0
-        end
-    ) as vl_credit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'boleto' then payment_value
-            else 0
-        end
-    ) as vl_boleto,
-    1.0 * sum(
-        case
-            when payment_type = 'debit_card' then payment_value
-            else 0
-        end
-    ) as vl_debit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'voucher' then payment_value
-            else 0
-        end
-    ) as vl_voucher,
-    1.0 * sum(
-        case
-            when payment_type = 'credit_card' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) / sum(qtdPedidoMeioPagamento) as pct_qtd_credit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'boleto' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) / sum(qtdPedidoMeioPagamento) as pct_qtd_boleto,
-    1.0 * sum(
-        case
-            when payment_type = 'debit_card' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) / sum(qtdPedidoMeioPagamento) as pct_qtd_debit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'voucher' then qtdPedidoMeioPagamento
-            else 0
-        end
-    ) / sum(qtdPedidoMeioPagamento) as pct_qtd_voucher,
-    1.0 * sum(
-        case
-            when payment_type = 'credit_card' then payment_value
-            else 0
-        end
-    ) / sum(payment_value) as pct_vl_credit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'boleto' then payment_value
-            else 0
-        end
-    ) / sum(payment_value) as pct_vl_boleto,
-    1.0 * sum(
-        case
-            when payment_type = 'debit_card' then payment_value
-            else 0
-        end
-    ) / sum(payment_value) as pct_vl_debit_card,
-    1.0 * sum(
-        case
-            when payment_type = 'voucher' then payment_value
-            else 0
-        end
-    ) / sum(payment_value) as pct_vl_voucher
+    '2018-01-01' as dtReferencia,
+    t1.*,
+    t2.avgQtdParcelas,
+    t2.medianaQtdParcelas,
+    t2.maxQtdParcelas,
+    t2.minQtdParcelas
 from
-    tb_group
-GROUP BY
-    seller_id
+    tb_summary as t1
+left join tb_cartao as t2
+on t1.seller_id = t2.seller_id
