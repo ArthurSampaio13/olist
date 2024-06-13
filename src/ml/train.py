9584975289cae76b4f8775032fa9a5927b8028d7
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-13 13:01:21 -0300

    feat: treino

--- src/ml/train.py
+++ src/ml/train.py
@@ -1,15 +1,16 @@
 # %%
-from math import pi
 import pandas as pd
 import sqlalchemy
 import sqlite3
 
-from sklearn import model_selection, tree
+from sklearn import model_selection
 from sklearn import tree
 from sklearn import pipeline
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import scikitplot as skplt
 
 from feature_engine import imputation
-
 
 pd.set_option('display.max_rows', 1000)
 # %%
@@ -24,10 +25,10 @@ print("OK.")
 # %%
 # Sample - Back Test (OOT)
 
-dt_oot = df[df['dtReferencia']=='2018-01-01']
+dt_oot = df[df['dtReferencia'] == '2018-01-01']
 # %%
 
-df_train = df[df['dtReferencia']!='2018-01-01']
+df_train = df[df['dtReferencia'] != '2018-01-01']
 # %%
 # Definindo variaveis
 target = 'flChurn'
@@ -77,15 +78,50 @@ imputer_0 = imputation.ArbitraryNumberImputer(arbitrary_number=0, variables=miss
 
 # %%
 # Modeling
-model = tree.DecisionTreeClassifier()
+model = tree.DecisionTreeClassifier(min_samples_leaf=50)
 
 # %%
+# Pipeline
 model_pipe = pipeline.Pipeline([("Imputer -1", imputer_minus_1),
                                ("Imputer 0", imputer_0),
                                ("Decision Tree", model)])
 # %%
 model_pipe.fit(X_train, y_train)
 # %%
-model_pipe.predict(X_test)
-model_pipe.predict_proba(X_test)
+predict = model_pipe.predict(X_train)
 # %%
+probas = model_pipe.predict_proba(X_train)
+proba = probas[:,1]
+# %%
+skplt.metrics.plot_roc(y_train, probas)
+plt.show()
+# %%
+skplt.metrics.plot_ks_statistic(y_train, probas)
+plt.show()
+
+# %%
+probas_test = model_pipe.predict_proba(X_test)
+# %%
+skplt.metrics.plot_roc(y_test, probas_test)
+plt.show()
+# %%
+skplt.metrics.plot_ks_statistic(y_test, probas_test)
+plt.show()
+# %%
+probas_oot = model_pipe.predict_proba(X=dt_oot[features])
+
+# %%
+skplt.metrics.plot_roc(dt_oot[target], y_probas=probas_oot)
+plt.show()
+# %%
+skplt.metrics.plot_ks_statistic(dt_oot[target], probas_oot)
+plt.show()
+# %%
+fs_importance = model_pipe[-1].feature_importances_
+fs_cols = model_pipe[:-1].transform(X_train.head(1)).columns.tolist()
+
+pd.Series(fs_importance, index=fs_cols).sort_values(ascending=False)
+
+# %%
+skplt.metrics.plot_lift_curve(y_train, probas)
+plt.show()
