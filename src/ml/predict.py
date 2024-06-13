2d2ebe96dab043a96729f1b9c428530f9f8dfdd4
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-06-13 20:37:58 -0300

    deploy em batch

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -3,20 +3,34 @@ import mlflow
 import pandas as pd
 import sqlalchemy
 import sqlite3
+import datetime
 # %%
-model = mlflow.sklearn.load_model("models:/olist/latest")
-
+# Set MLflow tracking URI and load the model
+mlflow.set_tracking_uri('http://localhost:5000/')
+model = mlflow.sklearn.load_model("models:/olistt@olist")
 # %%
+# Prediction
 engine = sqlalchemy.create_engine("sqlite:///../../data/olist.db")
 conn = sqlite3.connect("../../data/olist.db")
-query = "SELECT * FROM ABT"
+query = "SELECT * FROM fs_join"
 df = pd.read_sql_query(query, conn)
-
 # %%
-predict = model.predict_proba(df[model.feature_names_in_])[:,1]
-predict
+# ETL
+predict = model.predict_proba(df[model.feature_names_in_])
+predict_0 = predict[:,0]
+predict_1 = predict[:,1]
+
+df_extract = df[['seller_id']].copy()
+df_extract['0'] = predict_0
+df_extract['1'] = predict_1
+
+df_extract = (df_extract.set_index('seller_id')
+                        .stack()
+                        .reset_index())
+
+df_extract.columns = ['seller_id', 'descClass', 'Score']
+df_extract['descModel'] = 'Churn Vendedor'
+df_extract['dtScore'] = datetime.datetime.now()
 # %%
-
-
 
 # %%
